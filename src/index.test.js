d4f7d710a91edeaa907e9c5786bec6b5b6454ac9
Rares <mindricanmihai@gmail.com>
2018-01-29 07:41:40 +0200

    - worked with mock data

--- src/index.test.js
+++ src/index.test.js
@@ -11,11 +11,11 @@ describe('Our first test',()=>{
 
 
 describe('index.htm',(done)=>{
-  it('should say hello',()=>{
+  it('should have h1 that says users',()=>{
     const index = fs.readFileSync('./src/index.html','utf-8');
     jsdom.env(index,function (err,window) {
       const h1=window.document.getElementsByTagName('h1')[0];
-      expect(h1.innnerHtml).equal("Hello World!");
+      expect(h1.innnerHtml).equal("Users");
       done();
       window.close();
     });
